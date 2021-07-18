579c1099c7d3a4e419f6e998cc322e17af5c9af4
fesgic <festusgichohi2@gmail.com>
2021-07-18 22:51:53 +0300

    capture network traffic from interfaces

---
+++ capture/capture.py
@@ -0,0 +1,15 @@
+import logging
+import os
+
+logging.getLogger("scapy").setLevel(logging.CRITICAL)
+from scapy.all import *
+
+
+def packet_capture():
+    pkts = sniff(iface=interface, prn=lambda x: x.summary())
+    wrpcap(file, pkts)
+    print(pkts)
+
+
+def permissions():
+    os.system(f"chmod 777 {file}")
