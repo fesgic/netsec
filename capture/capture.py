50fdd1388fd20fb9f446f9df766e8835cbc020e7
fesgic <festusgichohi2@gmail.com>
2021-09-02 22:53:07 +0300

    refractoring

--- capture/capture.py
+++ capture/capture.py
@@ -1,15 +1,17 @@
 import logging
 import os
+import sys
 
 logging.getLogger("scapy").setLevel(logging.CRITICAL)
 from scapy.all import *
 
 
-def packet_capture():
-    pkts = sniff(iface=interface, prn= lambda  x: x.summary())
+def packet_capture(pkt):
+    pkt.show()
     wrpcap(file, pkts)
-
 
 
 def permissions():
     os.system(f"chmod 777 {file}")
+
+sniff(iface=interface, prn=packet_capture, store=0)
